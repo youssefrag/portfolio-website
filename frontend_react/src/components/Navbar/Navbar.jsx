6364a1897acc6c7b6c17026fd3c2604559197b5a
Youssef Ragab <ragab.youssef95@gmail.com>
2022-10-31 17:39:37 -0400

    basic toggle functionality

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Navbar.scss";
+
+import { HiOutlineViewList, HiOutlineX } from "react-icons/hi";
 
 import { images } from "../../constants";
 
 export const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -12,11 +16,19 @@ export const Navbar = () => {
       </div>
       <ul className="app__navbar-links">
         {["home", "about", "projects", "contact"].map((item) => (
-          <li className="app__flex p-text" key={`link-${item}`}>
+          <li key={`link-${item}`}>
             <a href={`#${item}`}>{item}</a>
           </li>
         ))}
       </ul>
+
+      <div className="app__navbar-menu">
+        {toggle ? (
+          <HiOutlineX onClick={() => setToggle(false)} />
+        ) : (
+          <HiOutlineViewList onClick={() => setToggle(true)} />
+        )}
+      </div>
     </nav>
   );
 };
