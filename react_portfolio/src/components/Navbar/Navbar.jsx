c436d4d3051b7ae9fc4743feebe51c86d102311b
Youssef Ragab <ragab.youssef95@gmail.com>
2022-11-03 10:48:41 -0400

    finish responsive navigation

--- react_portfolio/src/components/Navbar/Navbar.jsx
+++ react_portfolio/src/components/Navbar/Navbar.jsx
@@ -1,59 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Navbar.scss";
+
+import { HiMenuAlt4, HiX } from "react-icons/hi";
 
 const Navbar = (props) => {
   const { page, setPage, navClass } = props;
 
+  const [toggle, setToggle] = useState(false);
+
   if (navClass === "fixed") {
     return (
-      <nav className="section-nav">
-        <ul className="app__navbar-links">
-          {["home", "about", "projects", "contact"].map((item) => (
-            <li className="app__flex p-text" key={`link-${item}`}>
-              <div />
-              {page === item ? (
-                <a
-                  onClick={() => setPage(item)}
-                  href={`#${item}`}
-                  style={{ color: "#f03e3e" }}
-                >
-                  {item}
-                </a>
-              ) : (
-                <a onClick={() => setPage(item)} href={`#${item}`}>
-                  {item}
-                </a>
-              )}
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <>
+        <nav className="section-nav">
+          <ul className="app__navbar-links">
+            {["home", "about", "projects", "contact"].map((item) => (
+              <li className="app__flex p-text" key={`link-${item}`}>
+                <div />
+                {page === item ? (
+                  <a
+                    onClick={() => setPage(item)}
+                    href={`#${item}`}
+                    style={{ color: "#f03e3e" }}
+                  >
+                    {item}
+                  </a>
+                ) : (
+                  <a onClick={() => setPage(item)} href={`#${item}`}>
+                    {item}
+                  </a>
+                )}
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className="app__navbar-menu">
+          <HiMenuAlt4 onClick={() => setToggle(true)} />
+          {toggle && (
+            <div>
+              <HiX onClick={() => setToggle(false)} />
+              <ul>
+                {["home", "about", "projects", "contact"].map((item) => (
+                  <li
+                    className="app__flex p-text"
+                    key={`link-${item}`}
+                    onClick={() => setToggle(false)}
+                  >
+                    <a onClick={() => setPage(item)} href={`#${item}`}>
+                      {item}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      </>
     );
   } else if (navClass === "sticky") {
     return (
-      <nav className="section-nav-sticky">
-        <ul className="app__navbar-links">
-          {["home", "about", "projects", "contact"].map((item) => (
-            <li className="app__flex p-text" key={`link-${item}`}>
-              <div />
-              {page === item ? (
-                <a
-                  onClick={() => setPage(item)}
-                  href={`#${item}`}
-                  style={{ color: "#f03e3e" }}
-                >
-                  {item}
-                </a>
-              ) : (
-                <a onClick={() => setPage(item)} href={`#${item}`}>
-                  {item}
-                </a>
-              )}
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <>
+        <nav className="section-nav-sticky">
+          <ul className="app__navbar-links">
+            {["home", "about", "projects", "contact"].map((item) => (
+              <li className="app__flex p-text" key={`link-${item}`}>
+                <div />
+                {page === item ? (
+                  <a
+                    onClick={() => setPage(item)}
+                    href={`#${item}`}
+                    style={{ color: "#f03e3e" }}
+                  >
+                    {item}
+                  </a>
+                ) : (
+                  <a onClick={() => setPage(item)} href={`#${item}`}>
+                    {item}
+                  </a>
+                )}
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className="app__navbar-menu">
+          <HiMenuAlt4 onClick={() => setToggle(true)} />
+          {toggle && (
+            <div>
+              <HiX onClick={() => setToggle(false)} />
+              <ul>
+                {["home", "about", "projects", "contact"].map((item) => (
+                  <li
+                    className="app__flex p-text"
+                    key={`link-${item}`}
+                    onClick={() => setToggle(false)}
+                  >
+                    <a onClick={() => setPage(item)} href={`#${item}`}>
+                      {item}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      </>
     );
   }
 };
